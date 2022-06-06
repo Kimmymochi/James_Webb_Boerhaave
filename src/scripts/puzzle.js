a45c20aa708d7c1f81980b05589290d3ffae3ab4
TijsRuigrok <47102492+TijsRuigrok@users.noreply.github.com>
2022-06-06 12:02:08 +0200

    Found positions for snapping points

--- src/scripts/puzzle.js
+++ src/scripts/puzzle.js
@@ -6,16 +6,18 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import backpanel from '../models/backpanel.gltf'
 import BUS from '../models/BUS.gltf'
 import goldPlating from '../models/gold_plating.gltf'
-// import ISIS from '../models/ISIS.gltf'
+import ISIS from '../models/ISIS.gltf'
 import secondaryMirror from '../models/secondary_mirror.gltf'
 import solarPanels from '../models/solar_panels.gltf'
 import sunscreens from '../models/sunscreens.gltf'
 
 import DragControls from 'three-dragcontrols'
 
+
 const scene = new THREE.Scene();
 
 // RENDERER
+// ----------------------------------------------------------------------
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -25,13 +27,16 @@ document.body.appendChild( renderer.domElement );
 
 
 // CAMERA
+// ----------------------------------------------------------------------
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 const orbitControls = new OrbitControls( camera, renderer.domElement );
 camera.position.set( 0, 20, 100 );
+orbitControls.enableZoom = false;
 orbitControls.update();
 
 
 // LIGHTS
+// ----------------------------------------------------------------------
 const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add( light );
 
@@ -42,9 +47,9 @@ dirLight.castShadow = true;
 
 
 // INITIATION
-const draggableObjects = [];
+// ----------------------------------------------------------------------
+let draggableObjects = [];
 const meshes = [backpanel, BUS, goldPlating, secondaryMirror, solarPanels, sunscreens];
-// const meshes = [backpanel];
 
 let snappingPointsData = [];
 let partsData = [];
@@ -53,30 +58,48 @@ const snappingPointRadius = 1;
 
 let collisionsEnabled = false;
 
+const partPositions = [
+    new THREE.Vector3(23, -4, 20),
+    new THREE.Vector3(-22, -13, 15),
+    new THREE.Vector3(20, 27, -14),
+    new THREE.Vector3(23, 27, 21),
+    new THREE.Vector3(-16, 24, -27),
+    new THREE.Vector3(-40, 0, -37),
+];
+
+// TODO: Find correct snapping points positions instead of guessing
+const SPPositions = [
+    new THREE.Vector3(0.60, 19.15, -0.75), // backpanel
+    new THREE.Vector3(0.60, -0.23, -1.24), // BUS
+    new THREE.Vector3(0.60, 18.81, 2.03), // gold plating
+    new THREE.Vector3(0.60, 20.56, 12.77), // secondary mirror
+    new THREE.Vector3(0.60, 5.79, -17.47), // solar panels
+    new THREE.Vector3(0.60, 5.22, 1.15) // sunscreens
+];
+
 init();
 
 function init() {
-    // Create a draggable part for all 3D models
+
     for (let i = 0; i < meshes.length; i++) {
-        let draggablePart = addDraggablePart(meshes[i]);
+
+        // Create a draggable part for all 3D models
+        let draggablePart = addDraggablePart(meshes[i], partPositions[i]);
         scene.add(draggablePart);
         draggableObjects.push(draggablePart);
-    }
 
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(0.05, -10.45, -2.18)); // backpanel
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(0.21, -6.34, -0.58)); // BUS
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(-0.04, 10.12, 4.64)); // gold plating
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(0, 11.70, 15.54)); // secondary mirror
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(0.01, 0.03, -16.95)); // solar panels
-    addSnappingPoint(snappingPointRadius, new THREE.Vector3(0, -0.21, 1.66)); // sunscreens
+        // Create a snapping point for each part
+        addSnappingPoint(snappingPointRadius, SPPositions[i]);
+    }
 
     animate();
 }
 
-let currentlyDragging = false;
 
 // DRAG & DROP
+// ----------------------------------------------------------------------
 const dragControls = new DragControls( draggableObjects, camera, renderer.domElement );
+let currentlyDragging = false;
 
 dragControls.addEventListener( 'dragstart', function ( event )
 {
@@ -106,26 +129,20 @@ function drawBox(objectWidth, objectHeight, objectDepth, material)
     return box;
 }
 
-function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function addDraggablePart(mesh)
+function addDraggablePart(mesh, pos)
 {
     let group = new THREE.Group();
     const gltfLoader = new GLTFLoader();
 
     gltfLoader.load( mesh, ( gltf ) =>
     {
-        let mesh = gltf.scene;
-        mesh.scale.set( 3, 3, 3);
+        let model = gltf.scene;
+        model.scale.set(3, 3, 3);
 
-        // TODO: shadows not working
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
+        model.castShadow = true;
+        model.receiveShadow = true; // TODO: shadows not working
 
-        const boundingBox = new THREE.Box3().setFromObject( mesh );
-
+        const boundingBox = new THREE.Box3().setFromObject( model );
         let meshSize = new THREE.Vector3();
         let meshPosition = new THREE.Vector3();
         boundingBox.getSize(meshSize);
@@ -140,28 +157,25 @@ function addDraggablePart(mesh)
         hitBox.geometry.computeBoundingBox();
 
         partsData.push({
+            model: model,
             mesh: hitBox,
             boundingBox: boundingBox,
             snappingPoint: null,
         },);
 
-        mesh.position.set(-meshPosition.x, -meshPosition.y, -meshPosition.z);
-        console.log(mesh.position);
-        // console.log(mesh.position);
+        model.position.set(-meshPosition.x, -meshPosition.y, -meshPosition.z);
 
         group.add(hitBox);
-        hitBox.add(mesh);
+        hitBox.add(model);
 
-        // hitBox.position.set(
-        //     getRandomNumber(100, 50),
-        //     getRandomNumber(100, 50),
-        //     getRandomNumber(100, 50));
+        hitBox.position.set(pos.x, pos.y, pos.z);
     });
     return group;
 }
 
 
-// SNAPPING POINT
+// SNAPPING POINTS & COLLISION DETECTION
+// ----------------------------------------------------------------------
 function addSnappingPoint(radius, pos)
 {
     const snappingPointMesh = new THREE.Mesh(
@@ -182,131 +196,156 @@ function addSnappingPoint(radius, pos)
         mesh: snappingPointMesh,
         boundingBox: snappingPointBB,
         snappedObject: null,
-        hasRecentlyCollided: false,
     });
 
     return snappingPointMesh;
 }
 
+let closestPartDistance = 10000000;
+let closestPart = null;
+let closestSP = null;
 
-const snappingDistance = 10;
+const snappingDistance = 20;
 const toleranceDistance = 0.1;
 
 function checkCollisions()
 {
-    let closestPartDistance = 10000000;
-    let closestPart = null;
-    let closestSP = null;
-
     for (let SPIndex = 0; SPIndex < snappingPointsData.length; SPIndex++)
     {
-        snappingPointsData[SPIndex].mesh.material.color.setHex(0xffffff);
+        SPDefaultState(snappingPointsData[SPIndex]);
 
         if (snappingPointsData[SPIndex].snappedObject == null )
         {
-            for (let partsIndex = 0; partsIndex < partsData.length; partsIndex++)
-            {
-                if(partsData[partsIndex].snappingPoint == null)
-                {
-                    const partPos = partsData[partsIndex].mesh.position;
-                    const SPPos = snappingPointsData[SPIndex].mesh.position;
-                    const distance = partPos.distanceTo(SPPos);
-
-                    if(distance < closestPartDistance)
-                    {
-                        closestPartDistance = distance;
-                        closestPart = partsData[partsIndex];
-                        closestSP = snappingPointsData[SPIndex];
-                    }
-                }
-            }
+            findClosestPart(snappingPointsData[SPIndex]);
         }
         else
         {
-            const partPos = snappingPointsData[SPIndex].snappedObject.mesh.position;
-            const SPPos = snappingPointsData[SPIndex].mesh.position;
-            const distance = partPos.distanceTo(SPPos);
-
-            if (distance >= toleranceDistance)
-            {
-                snappingPointsData[SPIndex].snappedObject.snappingPoint = null;
-                snappingPointsData[SPIndex].snappedObject = null;
-            }
+            ensureTolerableDistance(snappingPointsData[SPIndex]);
         }
     }
     if(closestPartDistance <= snappingDistance)
     {
-        closestSP.mesh.material.color.setHex(0xeb4034);
-        if (!currentlyDragging)
-        {
-            closestSP.snappedObject = closestPart;
-            closestPart.snappingPoint = closestSP;
+        SPHoverState(closestSP);
+        snapPartToSP();
+    }
 
-            const snappingPointPos = closestSP.mesh.position;
-            closestPart.mesh.position.set(snappingPointPos.x, snappingPointPos.y, snappingPointPos.z);
+    closestPartDistance = 10000000;
+    closestPart = null;
+    closestSP = null;
+}
+
+function findClosestPart(snappingPointData)
+{
+    for (let partsIndex = 0; partsIndex < partsData.length; partsIndex++)
+    {
+        if(partsData[partsIndex].snappingPoint == null)
+        {
+            const partPos = partsData[partsIndex].mesh.position;
+            const SPPos = snappingPointData.mesh.position;
+            const distance = partPos.distanceTo(SPPos);
+
+            if(distance < closestPartDistance)
+            {
+                closestPartDistance = distance;
+                closestPart = partsData[partsIndex];
+                closestSP = snappingPointData;
+            }
         }
     }
 }
 
-// function checkCollisions()
-// {
-//     // Loop through all parts
-//     for (let partsIndex = 0; partsIndex < partsData.length; partsIndex++)
-//     {
-//         for (let SPIndex = 0; SPIndex < snappingPointsData.length; SPIndex++)
-//         {
-//             let isIntersecting = partsData[partsIndex].boundingBox.intersectsSphere(
-//                 snappingPointsData[SPIndex].boundingBox);
-//
-//             // Check if any parts are intersecting with a snapping point
-//             if (isIntersecting || snappingPointsData[SPIndex].hasRecentlyCollided)
-//             {
-//                 snappingPointsData[SPIndex].snappedObject = partsData[partsIndex].mesh;
-//
-//                 if (!snappingPointsData[SPIndex].hasRecentlyCollided)
-//                 {
-//                     // Place part in center of snapping point
-//                     let snappingPointPos = snappingPointsData[SPIndex].boundingBox.center;
-//                     partsData[partsIndex].mesh.position.set(snappingPointPos.x, snappingPointPos.y, snappingPointPos.z);
-//
-//                     //
-//                     dragControls.enabled = false;
-//                     setTimeout(function ()
-//                     {
-//                         dragControls.enabled = true;
-//                     }, 1000);
-//                 }
-//                 snappingPointsData[SPIndex].hasRecentlyCollided = true;
-//             }
-//
-//             if(!isIntersecting)
-//             {
-//                 snappingPointsData[SPIndex].hasRecentlyCollided = false;
-//                 snappingPointsData[SPIndex].snappedObject = null;
-//             }
-//         }
-//     }
-// }
+// Makes sure that snappedObjects are still located within tolerable distance of snapping point
+// otherwise it will remove the link
+function ensureTolerableDistance(snappingPointData)
+{
+    const partPos = snappingPointData.snappedObject.mesh.position;
+    const SPPos = snappingPointData.mesh.position;
+    const distance = partPos.distanceTo(SPPos);
 
+    if (distance >= toleranceDistance)
+    {
+        snappingPointData.snappedObject.snappingPoint = null;
+        snappingPointData.snappedObject = null;
+    }
+}
+
+// Locks closest part to closest snapping point
+function snapPartToSP()
+{
+    if (!currentlyDragging)
+    {
+        closestSP.snappedObject = closestPart;
+        closestPart.snappingPoint = closestSP;
+
+        const snappingPointPos = closestSP.mesh.position;
+        closestPart.mesh.position.set(snappingPointPos.x, snappingPointPos.y, snappingPointPos.z);
+    }
+}
+
+function SPHoverState(SP)
+{
+    SP.mesh.material.transparent = false;
+
+    const scale = snappingPointRadius * 1.5;
+    SP.mesh.scale.set(scale, scale, scale);
+}
+
+function SPDefaultState(SP)
+{
+    SP.mesh.material.transparent = true;
+
+    SP.mesh.scale.set(snappingPointRadius, snappingPointRadius, snappingPointRadius);
+}
+
+
+//  ANIMATE
+//  ----------------------------------------------------------------------
 function animate()
+{
+    updatePartsBBLocation();
+    // partsFloatAnimation();
+    if (collisionsEnabled) checkCollisions();
+
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+}
+
+// Updates the location of parts bounding box so it will
+// stay in the right position when it is dragged
+function updatePartsBBLocation()
 {
     for (let partsIndex = 0; partsIndex < partsData.length; partsIndex++)
     {
         partsData[partsIndex].boundingBox.copy(partsData[partsIndex].mesh.geometry.boundingBox)
             .applyMatrix4(partsData[partsIndex].mesh.matrixWorld);
     }
-
-    if (collisionsEnabled) checkCollisions();
-
-    renderer.render( scene, camera );
-    requestAnimationFrame( animate );
-
-    // for (let i = 0; i < draggableObjects.length; i++)
-    // {
-    //     const position = draggableObjects[i].position;
-    //     const speed = 0.002;
-    //     if(position.x < 100) position.set(position.x + speed, position.y + speed, position.z + speed);
-    // }
 }
+
+// Makes parts float like they would in space
+function partsFloatAnimation()
+{
+    for (let i = 0; i < draggableObjects.length; i++)
+    {
+        const position = draggableObjects[i].position;
+        const speed = 0.002;
+        if(position.x < 100) position.set(position.x + speed, position.y + speed, position.z + speed);
+    }
+}
+
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+
+    console.log("-----------------------------------------------------");
+
+    for (let partsIndex = 0; partsIndex < partsData.length; partsIndex++)
+    {
+        console.log(partsData[partsIndex].mesh.position);
+    }
+}
+
+
+
 
 

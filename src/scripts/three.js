296d63bf60964ddb6b301d649a26f15223001866
TijsRuigrok <47102492+TijsRuigrok@users.noreply.github.com>
2022-06-09 09:06:30 +0200

    Added fonts

--- src/scripts/three.js
+++ src/scripts/three.js
@@ -1,3 +1,5 @@
+import lato from '../fonts/Lato_Regular.json';
+
 const THREE = require('three');
 
 const scene = new THREE.Scene();
@@ -7,18 +9,16 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+const fontLoader = new THREE.FontLoader();
+fontLoader(lato)
+
 
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 }
 animate();
+
+
 

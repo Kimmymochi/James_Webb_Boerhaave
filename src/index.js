da084fca50dbe85e2b0cf5a7d92c06738a138e1f
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2022-06-14 03:18:20 +0200

    quotes working 🎉
    - all text and images added
    - images change quote text
    
    to do:
    - make responsive for bigger screens
    - hover animations?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './scripts/three.js';
+// import './scripts/three.js';
+import './scripts/quotes.js'
 import './styles/main.scss';
 
---
+++ src/scripts/quotes.js
@@ -0,0 +1,133 @@
+const THREE = require('three');
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import textData from '../data/text.json';
+const quotesData = textData.text.quotes;
+
+const quote = document.getElementById("js--quote");
+const quoteText = document.getElementById('js--quoteText');
+const quotePerson = document.getElementById('js--quotePerson');
+const quoteImages = document.querySelectorAll('.quote__imagebox__image');
+
+let camera;
+let controls;
+let scene;
+let renderer;
+
+init();
+animate();
+
+function init() {
+    // Camera
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 2, 2000);
+    camera.position.set(0, 0, 5);
+
+    // Scene
+    scene = new THREE.Scene();
+
+    // Lighting
+    //sun lighting
+    const sun = new THREE.PointLight( 0xffffff , 1, 500 );
+    sun.position.set( 20, -20, 0 );
+    sun.castShadow = true;
+    sun.shadow.radius = 2;
+    scene.add( sun );
+    const sun2 = new THREE.PointLight( 0xffffff , 1, 500 );
+    sun2.position.set( 20, 10, 0 );
+    sun2.castShadow = true;
+    sun2.shadow.radius = 2;
+    scene.add( sun2 );
+
+    //general lighting
+    const sceneLight = new THREE.AmbientLight(0xffffb8, 0.2);
+    scene.add(sceneLight);
+
+    // Renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x333333, 1);
+    document.body.appendChild(renderer.domElement);
+
+    // Controls
+	controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableZoom = false;
+    
+    // Set intro text
+    setQuote(0)
+    quote.style.display = "flex";
+
+    // EventListeners
+    window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+
+// sets quote field to corresponding id number
+function setQuote(quote) {
+    let keys = Object.keys(quotesData);
+    let quoteData;
+
+    for( let i = 0; i < keys.length; i++) {
+        let item = quotesData[keys[i]];
+        
+        if(item.id == quote) {
+            quoteData = item;
+        }
+    }
+    changeQuote(quoteData.text, quoteData.name, quoteData.background)
+}
+
+// changes the text of the quote with a fade animation
+function changeQuote(text, name, background) {
+    let textQuote = document.querySelector(".quote__box__text");
+    let nameQuote = document.querySelector(".quote__box__person--name");
+    let backgroundQuote = document.querySelector(".quote__box__person--background")
+
+    quoteText.style.opacity = 0;
+    quotePerson.style.opacity = 0;
+
+    setTimeout( function() {
+        textQuote.innerHTML = text;
+        nameQuote.innerHTML = name;
+        backgroundQuote.innerHTML = background;
+        quoteText.style.opacity = 1;
+        quotePerson.style.opacity = 1;
+    }, 500);
+}
+
+// will fire when images are clicked
+function quoteOnClick (event) {
+    // reset all previous active classes
+    for (let i = 0; i < quoteImages.length; i++) {
+        quoteImages[i].classList.remove("active");
+    }
+
+    // add active class to clicked image
+    event.classList.add("active");
+
+    // send the id of the clicked image to setQuote()
+    let clickedQuote = event.dataset.quoteId
+    setQuote(clickedQuote);
+}
+
+// prevent dragging image, bind function to image
+document.querySelectorAll(".quote__imagebox__image").forEach( item => {
+    item.setAttribute('draggable', false);
+    item.onclick = function(){quoteOnClick(item)};
+});
